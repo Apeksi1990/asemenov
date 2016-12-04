c657019b9241544cc869f0be2127c27dfd3730fe
Apeksi1990 <apeksi1990@gmail.com>
2016-12-04 21:28:36 +0300

    +calc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,12 @@
 package ru.job4j;
+
+/*
+*Class Calculate решение задачи части 001 урока1.
+*@autor asemenov
+*@sinse 04.12.2016
+*/
 public class Calculate{
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		System.out.println("Hello World");
 	}
 }

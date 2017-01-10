d038cb39c67aafea6c6fc347a537578014370e25
Apeksi1990 <apeksi1990@gmail.com>
2017-01-10 21:32:49 +0300

    getPosition

--- chapter_003/src/main/java/ru/asemenov/Palidrom/Palidrom.java
+++ chapter_003/src/main/java/ru/asemenov/Palidrom/Palidrom.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Objects;
 /**
  * Class Palidrom решение задачи части 003 урока 1.
  * @author asemenov
@@ -22,7 +21,7 @@ public class Palidrom {
             String palidrom = br.readLine();
             palidrom = palidrom.toLowerCase();
             String rotation = new StringBuffer(palidrom).reverse().toString();
-            return palidrom.length() == 5 & Objects.equals(palidrom, rotation);
+            return palidrom.length() == 5 & palidrom.equals(rotation);
         } catch (IOException exc) {
             System.out.println(exc);
         }

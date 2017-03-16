8965e967e3ad14f1a8c16548b04e7fb9f9d43e1f
Apeksi1990 <apeksi1990@gmail.com>
2017-03-16 19:22:19 +0300

    frog update

--- chapter_005/src/main/java/ru/asemenov/Frog/Frog.java
+++ chapter_005/src/main/java/ru/asemenov/Frog/Frog.java
@@ -1,5 +1,15 @@
 package ru.asemenov.Frog;
 
 public class Frog {
+    private Field field;
+    private Point start;
+    private Point finish;
 
+    private int min = 0;
+
+    public Frog(Field field, Point start, Point finish) {
+        this.field = field;
+        this.start = start;
+        this.finish = finish;
+    }
 }

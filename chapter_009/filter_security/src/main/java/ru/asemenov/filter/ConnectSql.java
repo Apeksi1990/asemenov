ddec2ee9863088a92c0810a7b9320cb9a4a36548
Apeksi1990 <apeksi1990@gmail.com>
2017-06-16 18:36:18 +0300

    connect update

--- chapter_009/filter_security/src/main/java/ru/asemenov/filter/ConnectSql.java
+++ chapter_009/filter_security/src/main/java/ru/asemenov/filter/ConnectSql.java
@@ -4,7 +4,6 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import ru.asemenov.filter.models.Role;
 import ru.asemenov.filter.models.User;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -20,16 +19,19 @@ public class ConnectSql {
     private static final ConnectSql instance = new ConnectSql();
 
     private ConnectSql() {
+        init();
     }
 
     public static ConnectSql getInstance() {
         return instance;
     }
 
+    private Connection connection;
+
     /**
      * Connection.
      */
-    private DataSource getDataSource(){
+    private void init() {
         Properties props = new Properties();
         InputStream fis;
         BasicDataSource ds = new BasicDataSource();
@@ -39,13 +41,16 @@ public class ConnectSql {
             props.load(fis);
         }catch(IOException e){
             e.printStackTrace();
-            return null;
         }
         ds.setDriverClassName(props.getProperty("DRIVER"));
         ds.setUrl(props.getProperty("URL"));
         ds.setUsername(props.getProperty("USERNAME"));
         ds.setPassword(props.getProperty("PASSWORD"));
-        return ds;
+        try {
+            connection = ds.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -55,10 +60,8 @@ public class ConnectSql {
      * @param email String.
      */
     public void addUser(String name, String login, String password,  String email, int role_id) {
-        Connection connection = null;
         PreparedStatement ps = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("INSERT INTO sec_users(name, login, password, email, create_date, role_id) VALUES (?,?,?,?,?,?)");
             ps.setString(1, name);
             ps.setString(2, login);
@@ -74,9 +77,6 @@ public class ConnectSql {
                 if (ps != null) {
                     ps.close();
                 }
-                if (connection != null) {
-                    connection.close();
-                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -90,10 +90,8 @@ public class ConnectSql {
      * @param email String.
      */
     public void editUser(String oldLogin, String name, String login, String email) {
-        Connection connection = null;
         PreparedStatement ps = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("UPDATE sec_users SET name = ?, login = ?, email = ? WHERE login = ?");
             ps.setString(1, name);
             ps.setString(2, login);
@@ -107,9 +105,6 @@ public class ConnectSql {
                 if (ps != null) {
                     ps.close();
                 }
-                if (connection != null) {
-                    connection.close();
-                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -121,12 +116,10 @@ public class ConnectSql {
      * @return List.
      */
     public List<User> getUser() {
-        Connection connection = null;
         List<User> users = new ArrayList<>();
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("SELECT u.id, u.name, u.login, u.password, u.email, u.role_id, r.name role_name from sec_users u LEFT JOIN roles r on u.role_id = r.id");
             rs = ps.executeQuery();
             while (rs.next()) {
@@ -147,9 +140,6 @@ public class ConnectSql {
                 if (ps != null) {
                     ps.close();
                 }
-                if (connection != null) {
-                    connection.close();
-                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -162,10 +152,8 @@ public class ConnectSql {
      * @param login String.
      */
     public void deleteUser(String login) {
-        Connection connection = null;
         PreparedStatement ps = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("DELETE FROM sec_users WHERE login = ?");
             ps.setString(1, login);
             ps.executeUpdate();
@@ -176,9 +164,6 @@ public class ConnectSql {
                 if (ps != null) {
                     ps.close();
                 }
-                if (connection != null) {
-                    connection.close();
-                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -186,12 +171,10 @@ public class ConnectSql {
     }
 
     public boolean checkPassword(String login, String password) {
-        Connection connection = null;
         boolean result = false;
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("SELECT password from sec_users where login = ?");
             ps.setString(1, login);
             rs = ps.executeQuery();
@@ -209,9 +192,6 @@ public class ConnectSql {
                 if (ps != null) {
                     ps.close();
                 }
-                if (connection != null) {
-                    connection.close();
-                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -220,12 +200,10 @@ public class ConnectSql {
     }
 
     public String getRole(String login) {
-        Connection connection = null;
         String result = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("SELECT r.name role_name from sec_users u LEFT JOIN roles r on u.role_id = r.id where login = ?");
             ps.setString(1, login);
             rs = ps.executeQuery();
@@ -241,9 +219,6 @@ public class ConnectSql {
                 if (ps != null) {
                     ps.close();
                 }
-                if (connection != null) {
-                    connection.close();
-                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -252,12 +227,10 @@ public class ConnectSql {
     }
 
     public Map<Integer, String> getRoles() {
-        Connection connection = null;
         Map<Integer, String> result = new HashMap<>();
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("SELECT * from roles");
             rs = ps.executeQuery();
             while (rs.next()) {
@@ -273,9 +246,6 @@ public class ConnectSql {
                 if (ps != null) {
                     ps.close();
                 }
-                if (connection != null) {
-                    connection.close();
-                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -284,10 +254,8 @@ public class ConnectSql {
     }
 
     public void setRole(String login, int role_id) {
-        Connection connection = null;
         PreparedStatement ps = null;
         try {
-            connection = getDataSource().getConnection();
             ps = connection.prepareStatement("UPDATE sec_users SET role_id = ? WHERE login = ?");
             ps.setInt(1, role_id);
             ps.setString(2, login);
@@ -298,9 +266,6 @@ public class ConnectSql {
             try {
                 if (ps != null) {
                     ps.close();
-                }
-                if (connection != null) {
-                    connection.close();
                 }
             } catch (SQLException e) {
                 e.printStackTrace();

3280ac92507019cd9bfc284540e532affd91cddf
Apeksi1990 <apeksi1990@gmail.com>
2017-07-09 10:37:41 +0300

    interface update

--- chapter_009/interface/src/main/java/ru/asemenov/filter/servlets/AuthFilter.java
+++ chapter_009/interface/src/main/java/ru/asemenov/filter/servlets/AuthFilter.java
@@ -15,13 +15,15 @@ public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
-        if (request.getRequestURI().contains("/signin")) {
+        System.out.println(request.getRequestURI());
+        if (request.getRequestURI().contains("/login.html") || request.getRequestURI().contains("/signin")) {
             chain.doFilter(req, resp);
         } else {
             HttpSession session = request.getSession();
+            System.out.println(session.getAttribute("login"));
             synchronized (session) {
                 if (session.getAttribute("login") == null) {
-                    ((HttpServletResponse)resp).sendRedirect(String.format("%s/signin", request.getContextPath()));
+                    ((HttpServletResponse)resp).sendRedirect(String.format("%s/login.html", request.getContextPath()));
                     return;
                 }
             }
--- chapter_009/interface/src/main/java/ru/asemenov/filter/servlets/SigninController.java
+++ chapter_009/interface/src/main/java/ru/asemenov/filter/servlets/SigninController.java
@@ -8,28 +8,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class SigninController extends HttpServlet {
 
-    @Override
+    /*@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/login.html").forward(req, resp);
-    }
+        req.getRequestDispatcher("/login.html").forward(req, resp);
+    }*/
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
+        resp.setContentType("text/html");
         if (ConnectSql.getInstance().checkPassword(login, password)) {
             HttpSession session = req.getSession();
             synchronized (session) {
                 session.setAttribute("login", login);
                 session.setAttribute("role", ConnectSql.getInstance().getRole(login));
             }
-            resp.sendRedirect(String.format("%s/", req.getContextPath()));
         } else {
-            req.setAttribute("error", "Credentional invalid");
-            doGet(req, resp);
+            PrintWriter out = new PrintWriter(resp.getOutputStream());
+            out.append(String.valueOf(false));
+            out.flush();
         }
     }
 }

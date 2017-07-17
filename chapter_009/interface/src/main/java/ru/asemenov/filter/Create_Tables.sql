acb3c72612d0333f047869fd39249669ce059f4d
Apeksi1990 <apeksi1990@gmail.com>
2017-07-17 14:15:52 +0300

    create table update

--- chapter_009/interface/src/main/java/ru/asemenov/filter/Create_Tables.sql
+++ chapter_009/interface/src/main/java/ru/asemenov/filter/Create_Tables.sql
@@ -12,3 +12,16 @@ email varchar(50) UNIQUE,
 create_date TimeStamp,
 role_id int references roles(id)
 );
+
+create table countries (
+  id serial primary key,
+  name varchar(30) UNIQUE NOT NULL
+);
+
+create table cities (
+  id serial primary key,
+  name varchar(30) UNIQUE NOT NULL,
+  country_id int references countries(id) NOT NULL
+);
+
+ALTER TABLE sec_users ADD city_id int references cities(id);

80cb3b016ef354d20190391a6598d8cd47adf4e5
Apeksi1990 <apeksi1990@gmail.com>
2017-09-02 11:22:28 +0300

    todo connect update

--- chapter_010/todo/src/main/java/ru/asemenov/HiberConnect.java
+++ chapter_010/todo/src/main/java/ru/asemenov/HiberConnect.java
@@ -9,10 +9,17 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public class HiberConnect {
-    private static HiberConnect ourInstance = new HiberConnect();
+    private static volatile HiberConnect instance = new HiberConnect();
 
     public static HiberConnect getInstance() {
-        return ourInstance;
+        if (instance == null) {
+            synchronized (HiberConnect.class) {
+                if (instance == null) {
+                    instance = new HiberConnect();
+                }
+            }
+        }
+        return instance;
     }
 
     private HiberConnect() {
@@ -23,15 +30,27 @@ public class HiberConnect {
      * @return list.
      */
     public List<Item> getAllItem() {
-        SessionFactory factory = new Configuration()
-                .configure()
-                .buildSessionFactory();
-        Session session = factory.openSession();
-        session.beginTransaction();
-        List<Item> items = session.createQuery("from Item").list();
-        session.getTransaction().commit();
-        session.close();
-        factory.close();
+        SessionFactory factory = null;
+        Session session = null;
+        List<Item> items = null;
+        try {
+            factory = new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+            session = factory.openSession();
+            session.beginTransaction();
+            items = session.createQuery("from Item").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            if (factory != null) {
+                factory.close();
+            }
+        }
         return items;
     }
 
@@ -40,19 +59,30 @@ public class HiberConnect {
      * @param text String.
      */
     public void addItem(String text) {
-        SessionFactory factory = new Configuration()
-                .configure()
-                .buildSessionFactory();
-        Session session = factory.openSession();
-        session.beginTransaction();
-        Item item = new Item();
-        item.setDesc(text);
-        item.setCreated(new Timestamp(System.currentTimeMillis()));
-        item.setDone(false);
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        factory.close();
+        SessionFactory factory = null;
+        Session session = null;
+        try {
+            factory = new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+            session = factory.openSession();
+            session.beginTransaction();
+            Item item = new Item();
+            item.setDesc(text);
+            item.setCreated(new Timestamp(System.currentTimeMillis()));
+            item.setDone(false);
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            if (factory != null) {
+                factory.close();
+            }
+        }
     }
 
     /**
@@ -61,16 +91,27 @@ public class HiberConnect {
      * @param state boolean.
      */
     public void editState(int id, boolean state) {
-        SessionFactory factory = new Configuration()
-                .configure()
-                .buildSessionFactory();
-        Session session = factory.openSession();
-        session.beginTransaction();
-        Item item = session.get(Item.class, id);
-        item.setDone(state);
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
-        factory.close();
+        SessionFactory factory = null;
+        Session session = null;
+        try {
+            factory = new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+            session = factory.openSession();
+            session.beginTransaction();
+            Item item = session.get(Item.class, id);
+            item.setDone(state);
+            session.update(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            if (factory != null) {
+                factory.close();
+            }
+        }
     }
 }
